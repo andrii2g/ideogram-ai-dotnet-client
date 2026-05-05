bff51a93d4f75b94f0cd5efc87abb04f587cd625
Andrey <andrii@a2g.name>
2026-05-06 00:54:27 +0300

    added available Aspect Ratios

--- src/Ideogram.Client/Constants/IdeogramAspectRatios.cs
+++ src/Ideogram.Client/Constants/IdeogramAspectRatios.cs
@@ -1,5 +1,48 @@
+using System;
+using System.Collections.Generic;
+
 namespace Ideogram.Client.Constants;
 
 public static class IdeogramAspectRatios
 {
+    public const string Ratio1x3 = "1x3";
+    public const string Ratio1x2 = "1x2";
+    public const string Ratio9x16 = "9x16";
+    public const string Ratio10x16 = "10x16";
+    public const string Ratio2x3 = "2x3";
+    public const string Ratio3x4 = "3x4";
+    public const string Ratio4x5 = "4x5";
+    public const string Ratio1x1 = "1x1";
+    public const string Ratio5x4 = "5x4";
+    public const string Ratio4x3 = "4x3";
+    public const string Ratio3x2 = "3x2";
+    public const string Ratio16x10 = "16x10";
+    public const string Ratio16x9 = "16x9";
+    public const string Ratio2x1 = "2x1";
+    public const string Ratio3x1 = "3x1";
+
+    public static readonly IReadOnlySet<string> All = new HashSet<string>(StringComparer.Ordinal)
+    {
+        Ratio1x3,
+        Ratio1x2,
+        Ratio9x16,
+        Ratio10x16,
+        Ratio2x3,
+        Ratio3x4,
+        Ratio4x5,
+        Ratio1x1,
+        Ratio5x4,
+        Ratio4x3,
+        Ratio3x2,
+        Ratio16x10,
+        Ratio16x9,
+        Ratio2x1,
+        Ratio3x1
+    };
+
+    public static string Normalize(string value)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(value);
+        return value.Trim().Replace(':', 'x');
+    }
 }
