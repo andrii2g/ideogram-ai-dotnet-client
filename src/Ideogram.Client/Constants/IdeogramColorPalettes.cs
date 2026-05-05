6fb21ee0de466c717c2ab9920ce0335e76edd932
Andrey <andrii@a2g.name>
2026-05-06 00:54:47 +0300

    added Color Pallets constants

--- src/Ideogram.Client/Constants/IdeogramColorPalettes.cs
+++ src/Ideogram.Client/Constants/IdeogramColorPalettes.cs
@@ -2,4 +2,24 @@ namespace Ideogram.Client.Constants;
 
 public static class IdeogramColorPalettes
 {
+    public const string Ember = "EMBER";
+    public const string Fresh = "FRESH";
+    public const string Jungle = "JUNGLE";
+    public const string Magic = "MAGIC";
+    public const string Melon = "MELON";
+    public const string Mosaic = "MOSAIC";
+    public const string Pastel = "PASTEL";
+    public const string Ultramarine = "ULTRAMARINE";
+
+    public static readonly IReadOnlySet<string> All = new HashSet<string>(StringComparer.Ordinal)
+    {
+        Ember,
+        Fresh,
+        Jungle,
+        Magic,
+        Melon,
+        Mosaic,
+        Pastel,
+        Ultramarine
+    };
 }
