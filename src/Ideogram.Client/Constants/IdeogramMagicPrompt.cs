dbdbe8d536cefc922e13c85581f0809322f05681
Andrey <andrii@a2g.name>
2026-05-06 00:55:05 +0300

    added Magic Prompt constants

--- src/Ideogram.Client/Constants/IdeogramMagicPrompt.cs
+++ src/Ideogram.Client/Constants/IdeogramMagicPrompt.cs
@@ -2,4 +2,14 @@ namespace Ideogram.Client.Constants;
 
 public static class IdeogramMagicPrompt
 {
+    public const string Auto = "AUTO";
+    public const string On = "ON";
+    public const string Off = "OFF";
+
+    public static readonly IReadOnlySet<string> All = new HashSet<string>(StringComparer.Ordinal)
+    {
+        Auto,
+        On,
+        Off
+    };
 }
