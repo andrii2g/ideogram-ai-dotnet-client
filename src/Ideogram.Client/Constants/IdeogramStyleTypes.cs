e2e8c634a7e664f26dd977aa067c915da452ce98
Andrey <andrii@a2g.name>
2026-05-06 00:57:50 +0300

    added style types

--- src/Ideogram.Client/Constants/IdeogramStyleTypes.cs
+++ src/Ideogram.Client/Constants/IdeogramStyleTypes.cs
@@ -1,5 +1,22 @@
+using System;
+using System.Collections.Generic;
+
 namespace Ideogram.Client.Constants;
 
 public static class IdeogramStyleTypes
 {
+    public const string Auto = "AUTO";
+    public const string General = "GENERAL";
+    public const string Realistic = "REALISTIC";
+    public const string Design = "DESIGN";
+    public const string Fiction = "FICTION";
+
+    public static readonly IReadOnlySet<string> All = new HashSet<string>(StringComparer.Ordinal)
+    {
+        Auto,
+        General,
+        Realistic,
+        Design,
+        Fiction
+    };
 }
