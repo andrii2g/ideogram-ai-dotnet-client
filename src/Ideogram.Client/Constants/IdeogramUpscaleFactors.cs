21778e586de640b5efd33606528f9b0647c4e653
Andrey <andrii@a2g.name>
2026-05-06 00:58:16 +0300

    added Upscale Factors

--- src/Ideogram.Client/Constants/IdeogramUpscaleFactors.cs
+++ src/Ideogram.Client/Constants/IdeogramUpscaleFactors.cs
@@ -1,5 +1,18 @@
+using System;
+using System.Collections.Generic;
+
 namespace Ideogram.Client.Constants;
 
 public static class IdeogramUpscaleFactors
 {
+    public const string X1 = "X1";
+    public const string X2 = "X2";
+    public const string X4 = "X4";
+
+    public static readonly IReadOnlySet<string> All = new HashSet<string>(StringComparer.Ordinal)
+    {
+        X1,
+        X2,
+        X4
+    };
 }
